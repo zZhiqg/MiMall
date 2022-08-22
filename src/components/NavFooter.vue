ab6d7e3920725bc96c364d87898e4702eab9d1f0
zZhiqg <zzhiqg@126.com>
2022-08-22 17:06:09 +0800

    项目目录结构设置

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nav-footer',
+  data () {
+    return {}
+  },
+  created () {},
+  methods: {}
+}
+
+</script>
+<style scoped>
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavHeader from '@/components/NavHeader.vue'
+import NavFooter from '@/components/NavFooter.vue'
+export default {
+  components: { NavHeader, NavFooter },
+  name: '',
+  data () {
+    return {}
+  },
+  created () {},
+  methods: {}
+}
+
+</script>
+<style scoped>
+</style>
