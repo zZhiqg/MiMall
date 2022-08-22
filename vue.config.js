d933bf00954c236a04baa17cbf19f579a699734c
zZhiqg <zzhiqg@126.com>
2022-08-22 15:52:12 +0800

    开发环境的代理

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'http://www.imooc.com',
+                changeOrigin: true,
+                pathRewrite: {
+                    '/api': ''
+                }
+            }
+        }
+    }
 })
