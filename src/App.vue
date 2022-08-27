d011a8a0733cac32bce7d7a59787897f72c6480b
zZhiqg <zzhiqg@126.com>
2022-08-27 17:46:54 +0800

    Nav-header组件（1）

--- src/App.vue
+++ src/App.vue
@@ -13,25 +13,14 @@ export default {
   components: {},
   data() {
     return {
-      res:{}
+     
     };
   },
   mounted() {
-    //本地集成mockjs实现数据mock
-    this.axios.get('/user/login').then((res)=>{
-      this.res = res;
-    });
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/reset.scss'
 </style>
