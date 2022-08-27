e64b59f60825c0c379b5f0c26d7c6bd70d312d2d
zZhiqg <zzhiqg@126.com>
2022-08-27 22:30:08 +0800

    Nav-header组件（2）

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -17,7 +17,29 @@
         </div>
       </div>
     </div>
-    <div class="nav-header"></div>
+    <div class="nav-header">
+        <div class="container">
+            <div class="header-logo">
+               <a href="/#/index"></a> 
+            </div>
+            <div class="header-menu">
+                <div class="item-menu">
+                    <span>小米手机</span>
+                    <div class="children"></div>
+                </div>
+                <div class="item-menu">
+                    <span>Redmi红米</span>
+                    <div class="children"></div>
+                </div>
+                <div class="item-menu">
+                    <span>电视</span>
+                    <div class="children"></div>
+                </div>
+            </div>
+            <div class="header-search"></div>
+            
+        </div>
+    </div>
   </div>
 </template>
 
@@ -28,6 +50,7 @@ export default {
 </script>
 
 <style lang ="scss">
+@import '../assets/scss/base.scss';
 .header {
   .nav-topbar {
     height: 39px;
@@ -35,9 +58,7 @@ export default {
     background-color: #333333;
 
     .container {
-      width: 1226px;
-      margin-right: auto;
-      margin-left: auto;
+      
       display: flex;
       justify-content: space-between;
       align-items: center;
@@ -61,6 +82,66 @@ export default {
           background-size: contain;
         }
       }
+    }
+  }
+  .nav-header{
+    .container{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 112px;
+        /* 可视化区域 */
+        .header-logo{
+            display: inline-block;
+            width: 55px;
+            height: 55px;
+            background-color: #ff6600;
+            a {
+                display: inline-block;
+                width: 110px;
+                height: 55px;
+                &::before{
+                    content:'';
+                    display: inline-block;
+                    width: 55px;
+                    height: 55px;
+                    background: url('../assets/imgs/mi-logo.png') no-repeat center;
+                    background-size: 55px;
+                    transition: .2s;
+                }
+                &::after{
+                    content:'';
+                    display: inline-block;
+                    width: 55px;
+                    height: 55px;
+                    background: url('../assets/imgs/mi-home.png') no-repeat center;
+                    background-size: 55px;
+                    
+                }
+                &:hover:before{
+                    margin-left: -55px;
+                    transition:  .2s;
+                }
+            }
+        }
+        .header-menu{
+            display: inline-block;
+            width: 200px;
+            padding-left: 209px;
+            .item-menu{
+                display: inline-block;
+                color: #333333;
+                font-weight: bold;
+                font-size: 16px;
+                line-height: 112px;
+                span{
+                    cursor: pointer;
+                }
+                &:hover{
+                    
+                }
+            }
+        }
     }
   }
 }
