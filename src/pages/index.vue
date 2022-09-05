ac714f8c96584ad0ea071011ef28044eb29368f0
zZhiqg <zzhiqg@126.com>
2022-09-05 21:56:38 +0800

    首页广告位实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -49,8 +49,16 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a v-bind:href="'/#/product/' + item.id" v-for="(item,index) in adsList" v-bind:key="index">
+          <img v-bind:src="item.img" alt="">  
+        </a>  
+      </div> 
+      <div class="banner">
+        <a v-bind:href="'/#/product/30'">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -134,6 +142,24 @@ export default {
             name:'移动4G专区'
           },
         ],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+      ],
+      adsList:[
+        {
+          id:"33",
+          img:"/imgs/ads/ads-1.png"
+        },
+        {
+          id:"48",
+          img:"/imgs/ads/ads-2.jpg"
+        },
+        {
+          id:"45",
+          img:"/imgs/ads/ads-3.png"
+        },
+        {
+          id:"47",
+          img:"/imgs/ads/ads-4.jpg"
+        },
       ]
     };
   },
@@ -222,6 +248,18 @@ export default {
         height: 100%;
       }
     }
+  }
+  .ads-box{
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+    a{
+      width: 296px;
+      height: 167px;      
+    }
+  }
+  .banner{
+    margin-bottom: 50px;
   }
 }
 </style>
